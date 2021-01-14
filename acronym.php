afcf1d1b792a6e810a7acf358627a386f850ff8f
ontanj <anton.jeppsson@outlook.com>
2021-01-14 20:31:13 +0100

    allow command line argument

--- acronym.php
+++ acronym.php
@@ -1,20 +1,24 @@
 <?php
 
-print "Utvecklar förkortningen PHP.\nHur noga vill du vara? ";
-
-$handle = fopen("php://stdin", "r");
-$line = trim(fgets($handle));
-fclose($handle);
+if (count($argv) == 2) {
+  $line = $argv[1];
+} else {
+  print "Utvecklar förkortningen PHP.\nHur noga vill du vara? ";
+  
+  $handle = fopen("php://stdin", "r");
+  $line = trim(fgets($handle));
+  fclose($handle);
+  print "\n";
+}
 
 if (is_numeric($line)) {
-  $no = intval($line);
-  print "\n";
+  $no = intval($line); 
   print "PHP står för:\n";
   print "PHP";
   for ($i = 0; $i < $no; $i += 1) {
     print ": Hypertext Preprocessor";
   }
+  print "\n";
 } else {
   trigger_error("\n$line är inte ett tal.\n", E_USER_ERROR);
 }
-
