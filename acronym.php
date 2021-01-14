c49ce2630aa2dfbc3c119708f0a648d4d77479e4
ontanj <anton.jeppsson@outlook.com>
2021-01-14 20:22:18 +0100

    remove recursion (enjoying it might be)

--- acronym.php
+++ acronym.php
@@ -4,23 +4,17 @@ print "Utvecklar förkortningen PHP.\nHur noga vill du vara? ";
 
 $handle = fopen("php://stdin", "r");
 $line = trim(fgets($handle));
+fclose($handle);
 
 if (is_numeric($line)) {
   $no = intval($line);
-  $expansion = expand($line);
   print "\n";
   print "PHP står för:\n";
-  print "$expansion\n";
+  print "PHP";
+  for ($i = 0; $i < $no; $i += 1) {
+    print ": Hypertext Preprocessor";
+  }
 } else {
   trigger_error("\n$line är inte ett tal.\n", E_USER_ERROR);
 }
 
-fclose($handle);
-
-function expand($no) {
-  if ($no <= 0) {
-    return "PHP";
-  } else {
-    return expand(--$no) . ": Hypertext Preprocessor";
-  }
-} 
